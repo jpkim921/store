80c5cc472b5addfdd483f8495e90ca9c63c07666
Jayon Kim <jpkim921@gmail.com>
2020-06-15 19:47:37 -0400

    fix add-to-cart on product page

--- carts/urls.py
+++ carts/urls.py
@@ -5,7 +5,7 @@ from .views import add_to_cart, remove_from_cart, checkout
 app_name = 'cart'
 
 urlpatterns = [
-    path('cart/<slug>', add_to_cart, name="cart"),
-    path('remove/<slug>', remove_from_cart, name="remove-cart"),
+    path('add-to-cart/<slug>', add_to_cart, name="add-to-cart"),
+    path('remove-from-cart/<slug>', remove_from_cart, name="remove-from-cart"),
     path('checkout/', checkout, name="checkout"),
 ]
