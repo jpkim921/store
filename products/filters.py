22bd76c1c32a676ee3ff9431b1802e8301e4dfee
Jayon Kim <jpkim921@gmail.com>
2020-06-17 15:49:42 -0400

    add some styling to filter

--- products/filters.py
+++ products/filters.py
@@ -7,7 +7,7 @@ from .models import Product
 class ProductFilter(django_filters.FilterSet):
     # name = django_filters.CharFilter(lookup_expr='iexact')
     name = CharFilter(field_name="name", lookup_expr='icontains', label="Product Name",
-                      widget=forms.TextInput(attrs={'class': 'form-control mr-sm-2'}))
+                      widget=forms.TextInput(attrs={'class': 'form-control mb-2 mr-sm-2'}))
     # category = MultipleChoiceFilter(choices=Product.category, field_name='category', label="Category",
     #                                 widget=forms.SelectMultiple(attrs={'class': 'form-control form-control-sm'}))
 
