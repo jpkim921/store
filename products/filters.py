f6fc786a4491572e6249694f10dc2a7b24244dd4
Jayon Kim <jpkim921@gmail.com>
2020-06-17 15:31:17 -0400

    add couple more product examples

--- products/filters.py
+++ products/filters.py
@@ -1,10 +1,16 @@
 import django_filters
+from django_filters import CharFilter, NumberFilter, MultipleChoiceFilter, ChoiceFilter
+from django import forms
 from .models import Product
 
 
 class ProductFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='iexact')
+    # name = django_filters.CharFilter(lookup_expr='iexact')
+    name = CharFilter(field_name="name", lookup_expr='icontains', label="Product Name",
+                      widget=forms.TextInput(attrs={'class': 'form-control mr-sm-2'}))
+    # category = MultipleChoiceFilter(choices=Product.category, field_name='category', label="Category",
+    #                                 widget=forms.SelectMultiple(attrs={'class': 'form-control form-control-sm'}))
 
     class Meta:
         model = Product
-        fields = ['name', 'category', 'price']
+        fields = ['name', 'category']
