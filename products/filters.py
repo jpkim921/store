4342e51de33fcd0eac0a91dcd447a261e586d732
Jayon Kim <jpkim921@gmail.com>
2020-06-18 12:36:47 -0400

    alter filter to use changed categories attribute

--- products/filters.py
+++ products/filters.py
@@ -1,16 +1,15 @@
 import django_filters
-from django_filters import CharFilter, NumberFilter, MultipleChoiceFilter, ChoiceFilter
+from django_filters import CharFilter, MultipleChoiceFilter, ChoiceFilter, ModelChoiceFilter
 from django import forms
-from .models import Product
+from .models import *
 
 
 class ProductFilter(django_filters.FilterSet):
-    # name = django_filters.CharFilter(lookup_expr='iexact')
     name = CharFilter(field_name="name", lookup_expr='icontains', label="Product Name",
                       widget=forms.TextInput(attrs={'class': 'form-control mb-2 mr-sm-2'}))
-    # category = MultipleChoiceFilter(choices=Product.category, field_name='category', label="Category",
-    #                                 widget=forms.SelectMultiple(attrs={'class': 'form-control form-control-sm'}))
+    categories = ModelChoiceFilter(queryset=Category.objects.all(), widget=forms.Select)
+                                    
 
     class Meta:
         model = Product
-        fields = ['name', 'category']
+        fields = ['name', 'categories']
--- products/views.py
+++ products/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
-
 from .models import Product
-
 from .filters import ProductFilter
-
 
 def Home(request):
     products = Product.objects.all()
-
     myFilter = ProductFilter(request.GET, queryset=products)
     products = myFilter.qs
 
