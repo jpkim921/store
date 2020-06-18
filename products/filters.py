a73ce1af52a2a1d0f041d48c4aa8e10170432d24
Jayon Kim <jpkim921@gmail.com>
2020-06-18 13:57:51 -0400

    add class to select field. add placeholder in name field

--- products/filters.py
+++ products/filters.py
@@ -6,9 +6,8 @@ from .models import *
 
 class ProductFilter(django_filters.FilterSet):
     name = CharFilter(field_name="name", lookup_expr='icontains', label="Product Name",
-                      widget=forms.TextInput(attrs={'class': 'form-control mb-2 mr-sm-2'}))
-    categories = ModelChoiceFilter(queryset=Category.objects.all(), widget=forms.Select)
-                                    
+                      widget=forms.TextInput(attrs={'placeholder': 'Enter product name:', 'class': 'form-control mb-2 mr-sm-2'}))
+    categories = ModelChoiceFilter(queryset=Category.objects.all(), widget=forms.Select(attrs={'class': 'form-control mb-2 mr-sm-2'}))
 
     class Meta:
         model = Product
